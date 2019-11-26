8284fd0bd49e218e95d30694e6bd6a0f83b469b2
Paul P T Bellosillo <lozinalaifer.3791@gmail.cm>
2019-11-25 16:39:39 -0800

    Added Classes for main functionality

---
+++ Main.java
@@ -0,0 +1,13 @@
+/**
+ * main.java
+ * 
+ * Main Class for calling and creating a 
+ * 
+ * Authors: Dylan Pascua and Paul Patrick Bellosillo 
+ * Date: December 2, 2019
+ * 
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
